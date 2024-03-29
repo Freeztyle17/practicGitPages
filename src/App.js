a527b1e72b974f962f920c35f71c16d8548351bc
Freeztyle17 <fedorov.alexey2018@yandex.ru>
2024-03-29 11:11:20 +0400

    kuku

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       HALLO @@@@@@@@ 11111 2222222222
+       HALLO @@@@@@@@ 11111 222222
       </header>
     </div>
   );
